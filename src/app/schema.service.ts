450803c4354dffe5e2f2b07075993bdf8b83705d
JustifiedFlaw <ebbytes@gmail.com>
2022-01-06 10:47:20 -0500

    added add, rename and delete to schema service

--- src/app/schema.service.ts
+++ src/app/schema.service.ts
@@ -13,11 +13,28 @@ export class SchemaService {
     private http: HttpClient,
     private loginService: LoginService) { }
 
-  url(connectionId: number): string {
-    return `${environment.apiUrl}/connections/${connectionId}/schemas`;
+  url(connectionId: number, schema?: string): string {
+    let url = `${environment.apiUrl}/connections/${connectionId}/schemas`;
+    if (schema) {
+      url +=  + `/${schema}`;
+    }
+
+    return url;
   }
 
   getAll(connectionId: number): Observable<Schema[]> {
     return this.http.get<Schema[]>(this.url(connectionId), this.loginService.getAuthOptions());
+  }
+  
+  add(connectionId: number, schema: string): Observable<Object> {
+    return this.http.post(this.url(connectionId, schema), null, this.loginService.getAuthOptions());
+  }
+
+  rename(connectionId: number, schema: string, newName: string): Observable<Object> {
+    return this.http.put(this.url(connectionId, schema) + '?newName=' + newName, null, this.loginService.getAuthOptions());
+  }
+
+  drop(connectionId: number, schema: string): Observable<Object> {
+    return this.http.delete(this.url(connectionId, schema), this.loginService.getAuthOptions());
   }
 }
